e1d415574c5655f94db1adee65f0788c8f6f975c
CarlosPProjects <cg9822@gmail.com>
2024-02-22 23:04:17 +0100

    learning suspense and loader while fetching

--- src/components/ImageDetails.tsx
+++ src/components/ImageDetails.tsx
@@ -1,33 +1,17 @@
-"use client"
-
 import { getPhotoById } from "@/utils/unsplash";
-import { useEffect, useState } from "react";
 
 type Props = {
     imageId: string;
 }
 
-const ImageDetails: React.FC<Props> = ({ imageId }) => {
+const ImageDetails: React.FC<Props> = async ({ imageId }) => {
 
-    const [imageDetails, setImageDetails] = useState<ImageProps>();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await getPhotoById(imageId) as ImageProps;
-            setImageDetails(data);
-        }
-
-        fetchData();
-
-    }, [])
+    const data = await getPhotoById(imageId) as ImageProps;
 
     return (
-        imageDetails && (
-            <div>
-                {imageDetails.id}
-            </div>
-        )
+        <p>{data.description}</p>
     )
+
 }
 
 export default ImageDetails
--- src/components/ListPhotos.tsx
+++ src/components/ListPhotos.tsx
@@ -1,40 +1,31 @@
-"use client";
+"use client"
 
 import Image from "next/image";
-import { Suspense, useEffect, useState } from "react";
-import Loader from "./Loader";
-import { cn } from "@/utils/utils";
 import ImageDetails from "./ImageDetails";
+import { Suspense, useState } from "react";
+import Loader from "./Loader";
 
 interface Props {
   data: ImageProps[]
 }
 
 const ListPhotos: React.FC<Props> = ({ data }) => {
-  const [list, setList] = useState<ImageProps[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [openDetails, setOpenDetails] = useState<boolean>();
 
-  useEffect(() => {
-    const fetchPhotos = async () => {
-      setList(data);
-      setLoading(false);
-    };
+  const [imageId, setImageId] = useState<string>();
 
-    fetchPhotos();
-  }, []);
-
-  const handleClick = () => {
-    setOpenDetails(true);
+  // TODO: Refactor this handle function
+  const handleClick = (e: any) => {
+    const event = e.target.id;
+    setImageId(event);
   }
+
   return (
-    <div className={cn("w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4", loading ? "flex" : "grid")}>
-      {loading ? (
-        <Loader />
-      ) : (
-        list.map((image) => (
+    <>
+      <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4">
+        {data.map((image) => (
           <div onClick={handleClick} key={image.id} className="col-span-1 h-40 md:h-52 relative rounded-md overflow-hidden">
             <Image
+              id={image.id}
               src={image.urls.small}
               alt={image.alt_description}
               fill
@@ -43,17 +34,17 @@ const ListPhotos: React.FC<Props> = ({ data }) => {
               blurDataURL={image.urls.small}
               placeholder="blur"
             />
-            {openDetails && (
-              <div className="absolute top-0 left-0 right-0 bottom-0">
-                <Suspense>
-                  <ImageDetails imageId={image.id} />
-                </Suspense>
-              </div>
-            )}
           </div>
-        ))
+        ))}
+      </div>
+      {imageId && (
+        <div className="grid place-content-center absolute inset-0 bg-black bg-opacity-50">
+          <Suspense fallback={<Loader />}>
+            <ImageDetails imageId={imageId} />
+          </Suspense>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
